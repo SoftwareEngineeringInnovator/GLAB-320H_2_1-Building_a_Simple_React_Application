e88e9d5230eb01dafec2b082e15cf38d15ce0413
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-15 18:10:59 -0400

    create Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,16 @@ function Content() {
   return <p>This is my first React Application, wish me luck!</p>
 }
 
+function Footer() {
+  return <h5>Created by Fredy Chilito</h5>
+}
+
 export default function App() {
   return (
     <>
       <Header />
       <Content />
+      <Footer />
     </>
   )
 }

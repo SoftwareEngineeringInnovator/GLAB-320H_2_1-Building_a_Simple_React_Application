94153470c37b2f73619e9d310f401cdcd0aed835
SoftwareEngineeringInnovator <fredychilito@outlook.com>
2026-07-15 18:08:43 -0400

    create Content component

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,15 @@ function Header() {
   return <h1>Simple React Application</h1>
 }
 
+function Content() {
+  return <p>This is my first React Application, wish me luck!</p>
+}
+
 export default function App() {
   return (
-    <Header />
+    <>
+      <Header />
+      <Content />
+    </>
   )
 }
